fa1688f65e6976ec432c95e2de693b90e5f610a3
Hardeep Singh <hardeep_singh@jbhunt.com>
2018-07-23 15:24:52 -0500

    removed duplicate files

--- SagaOffline/actionTypes.js
+++
@@ -1,9 +0,0 @@
-
-const actionTypes = {
-  NETWORK_CHANGE_REACHABLE: '@@network-connectivity/NETWORK_CHANGE_REACHABLE',
-  NETWORK_CHANGE_UNREACHABLE: '@@network-connectivity/NETWORK_CHANGE_UNREACHABLE',
-  ADD_TO_OFFLINE_ACTION_QUEUE: '@@network-connectivity/ADD_TO_OFFLINE_ACTION_QUEUE',
-  REMOVE_FROM_OFFLINE_ACTION_QUEUE: '@@network-connectivity/REMOVE_FROM_OFFLINE_ACTION_QUEUE',
-};
-
-export default actionTypes;
--- SagaOffline/index.js
+++
@@ -1,7 +0,0 @@
-import offlineReducer from './reducer';
-import createSagaOfflineMiddleware from './offlineMiddleware';
-import Connectivity from './Connectivity';
-
-export { offlineReducer };
-export { Connectivity };
-export default createSagaOfflineMiddleware;
