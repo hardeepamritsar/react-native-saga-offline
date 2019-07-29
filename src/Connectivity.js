9404e88b8f9376b25d2966b4ba3257fb97939f98
Hardeep Singh <hardeep_singh@jbhunt.com>
2019-07-29 15:26:40 -0500

    custom domain to check reachability

--- src/Connectivity.js
+++ src/Connectivity.js
@@ -2,8 +2,10 @@ import Network from 'react-native-internet-reachability';
 import { networkRreachable, networkUnRreachable } from './actionCreators';
 
 export default class Connectivity {
-  constructor(store) {
+  constructor(store, timeout, domain) {
     this.store = store;
+    this.reachabilityTimeout = timeout ? timeout : 5000;
+    this.reachabilityDomain = domain ? domain : '8.8.8.8';
     this.previousConnectionState = null;
     setInterval(this.handleConnectivityChange.bind(this), 10000);
   }
@@ -20,10 +22,10 @@ export default class Connectivity {
   }
 
   async handleConnectivityChange() {
-    this.updateAvailability(await Network.isReachable());
+    this.updateAvailability(await Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain));
   }
 
   static async isConnected() {
-    return Network.isReachable();
+    return Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain);
   }
 }
