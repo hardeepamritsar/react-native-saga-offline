a5176c5acb1bcc912bd177dc59c23bcdf2aaf0c6
Hardeep Singh <hardeep_singh@jbhunt.com>
2019-07-31 15:02:26 -0500

    Singleton class for connectivity

--- src/Connectivity.js
+++ src/Connectivity.js
@@ -1,31 +1,34 @@
 import Network from 'react-native-internet-reachability';
 import { networkRreachable, networkUnRreachable } from './actionCreators';
 
-export default class Connectivity {
-  constructor(store, timeout, domain) {
-    this.store = store;
-    this.reachabilityTimeout = timeout ? timeout : 5000;
-    this.reachabilityDomain = domain ? domain : '8.8.8.8';
-    this.previousConnectionState = null;
-    setInterval(this.handleConnectivityChange.bind(this), 10000);
-  }
-
-  updateAvailability(isConnected) {
-    if (this.previousConnectionState !== isConnected) {
-      if (isConnected) {
-        this.store.dispatch(networkRreachable());
-      } else {
-        this.store.dispatch(networkUnRreachable());
-      }
+class Connectivity {
+    init(store, timeout, domain) {
+        this.store = store;
+        this.reachabilityTimeout = timeout ? timeout : 5000;
+        this.reachabilityDomain = domain ? domain : '8.8.8.8';
+        this.previousConnectionState = null;
+        setInterval(this.handleConnectivityChange.bind(this), 10000);
     }
-    this.previousConnectionState = isConnected;
-  }
-
-  async handleConnectivityChange() {
-    this.updateAvailability(await Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain));
-  }
-
-  static async isConnected() {
-    return Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain);
-  }
+    
+    updateAvailability(isConnected) {
+        if (this.previousConnectionState !== isConnected) {
+            if (isConnected) {
+                this.store.dispatch(networkRreachable());
+            } else {
+                this.store.dispatch(networkUnRreachable());
+            }
+        }
+        this.previousConnectionState = isConnected;
+    }
+    
+    async handleConnectivityChange() {
+        this.updateAvailability(await Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain));
+    }
+    
+    static async isConnected() {
+        return Network.isReachable(this.reachabilityTimeout, this.reachabilityDomain);
+    }
 }
+
+const instance = new Connectivity();
+export default instance;
