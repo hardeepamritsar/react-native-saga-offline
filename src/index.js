de4839c31e13bf2d50f5ad81e86203ef366d019a
Hardeep Singh <hardeep_singh@jbhunt.com>
2019-07-31 11:16:55 -0500

    exported action types

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import offlineReducer from './reducer';
 import createSagaOfflineMiddleware from './offlineMiddleware';
 import Connectivity from './Connectivity';
 import {removeFromOfflineActionQueue, addToOfflineActionQueue} from './actionCreators';
+import actionTypes from './actionTypes'
 
+export { actionTypes };
 export { offlineReducer };
 export { Connectivity };
 export { removeFromOfflineActionQueue, addToOfflineActionQueue };
